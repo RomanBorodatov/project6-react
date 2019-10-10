894bd6f068da1e5b5a618703c9892a7a843d3227
Roman Borodatov <roman.borodatov@vitd.co>
2019-10-10 23:04:47 +0300

    Updates

---
+++ precache-manifest.5e357968b5c70f008f19b40b2e7ac901.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aacd60df905d37b6353b2d1c8a282db",
+    "url": "//romanborodatov.github.io/project6-react/index.html"
+  },
+  {
+    "revision": "97aa03355f664be7a9ac",
+    "url": "//romanborodatov.github.io/project6-react/static/css/2.e71d8db5.chunk.css"
+  },
+  {
+    "revision": "0b61e54aea04a25c6a30",
+    "url": "//romanborodatov.github.io/project6-react/static/css/main.d9c47734.chunk.css"
+  },
+  {
+    "revision": "97aa03355f664be7a9ac",
+    "url": "//romanborodatov.github.io/project6-react/static/js/2.e15501d7.chunk.js"
+  },
+  {
+    "revision": "0b61e54aea04a25c6a30",
+    "url": "//romanborodatov.github.io/project6-react/static/js/main.5470e9d3.chunk.js"
+  },
+  {
+    "revision": "89fdd190bb07f5a47933",
+    "url": "//romanborodatov.github.io/project6-react/static/js/runtime-main.fa2571bc.js"
+  }
+]);
