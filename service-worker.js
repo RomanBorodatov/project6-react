ec06d5c9edc6cd7d27ab2a25279a26ba6eac4d26
Roman Borodatov <roman.borodatov@vitd.co>
2019-10-10 23:07:15 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "//romanborodatov.github.io/project6-react/precache-manifest.5e357968b5c70f008f19b40b2e7ac901.js"
+  "/project6-react/precache-manifest.195948581b37779ecdd2174a47319998.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("//romanborodatov.github.io/project6-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/project6-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
